c681bb5e87a7e4416bfb2421ea8db74efff63175
Francisco-Italo <65195244+Francisco-Italo@users.noreply.github.com>
2022-01-28 10:53:02 -0300

    Update Especialidade.java
    Update: a última versão estava gerando erros

--- Especialidade.java
+++ Especialidade.java
@@ -6,7 +6,26 @@
  * @version 1.0
  */
 enum Especialidade {
-    CARDIOLOGIA("cardiologia"), DERMATOLOGIA("dermatologia"), GINECOLOGIA("ginecologia"),
-    ENDOCRINOLOGIA("endocrinologia"), PNEUMOLOGIA("pneumologia"), PSIQUIATRIA("psiquiatria"),
-    PEDIATRIA("pediatria"), MEDICINA_VETERINARIA("medicina veterinária");
+    CARDIOLOGIA("Cardiologia"), DERMATOLOGIA("Dermatologia"), GINECOLOGIA("Ginecologia"),
+    ENDOCRINOLOGIA("Endocrinologia"), PNEUMOLOGIA("Pneumologia"), PSIQUIATRIA("Psiquiatria"),
+    PEDIATRIA("Pediatria"), MEDICINA_DENTARIA("Medicina dentária");
+
+    /**
+     * Variável que encapsula a string de cada atributo enumerado
+     */
+    private final String word;
+
+    /**
+     * Construtor da enum. Inicializa a string representativa dos tipos enumerados
+     * @param word a string que irá representar cada atributo do enum
+     */
+    Especialidade(String word){
+        this.word = word;
+    }
+
+    /**
+     * Getter dos atributos enumerados
+     * @return a string que representa o atributo enumerado selecionado
+     */
+    String getWord(){ return this.word; }
 }
