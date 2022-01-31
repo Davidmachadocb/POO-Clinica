67d2cfdc829536a68f0e2aa40015369e5b93d099
Francisco-Italo <65195244+Francisco-Italo@users.noreply.github.com>
2022-01-31 11:28:08 -0300

    Update Read.java

--- Read.java
+++ Read.java
@@ -6,7 +6,7 @@ import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
-class Read {
+class fileRead {
     public static void readWichFile(byte id){
         Locale.setDefault(Locale.US);
         String filePaciente = "/home/itsfrancisco/Área de Trabalho/Pacientes.txt";
@@ -26,51 +26,64 @@ class Read {
         File file = new File(fileSelected);
 
         try(Scanner inFile = new Scanner(file, StandardCharsets.UTF_8);){
+            int it = 0;
             switch(id){
                 case 1:
                     while(inFile.hasNext()){
                         String name = inFile.nextLine();
                         String endereco = inFile.nextLine();
-                        String numeroTel = inFile.next();
-                        String cpf = inFile.next();
+                        String numeroTel = inFile.nextLine();
+                        String cpf = inFile.nextLine();
                         byte dia = inFile.nextByte(); byte mes = inFile.nextByte(); int ano = inFile.nextInt();
-                        String sexo = inFile.next();
-                        String natu = inFile.next();
-                    
+                        inFile.nextLine();
+                        String sexo = inFile.nextLine();
+                        String natu = inFile.nextLine();
+                        
+                        System.out.println("---------------Registro #" + ++it + "---------------");
                         System.out.printf("Nome completo: %s%nEndereço: %s%nTelefone: %s%nCPF: %s%nData de nascimento: %d/%d/%d%nSexo: %s%nNaturalidade: %s%n",
                         name, endereco, numeroTel, cpf, dia, mes, ano, sexo, natu);
+                        System.out.println();
                     }
                 break;
                 case 2:
                     while(inFile.hasNext()){
                         String name = inFile.nextLine();
                         String endereco = inFile.nextLine();
-                        String numeroTel = inFile.next();
-                        String reg = inFile.next();
-                        String spec = inFile.next();
+                        String numeroTel = inFile.nextLine();
+                        String reg = inFile.nextLine();
+                        String spec = inFile.nextLine();
                     
+                        System.out.println("---------------Registro #" + ++it + "---------------");
                         System.out.printf("Nome completo: %s%nEndereço: %s%nTelefone: %s%nCRM: %s%nEspecialidade: %s%n",
                         name, endereco, numeroTel, reg, spec);
+                        System.out.println();
                     }
                 break;
                 case 3:
                     while(inFile.hasNext()){
-                        String cpfPac = inFile.next();
+                        String cpfPac = inFile.nextLine();
                         byte dia = inFile.nextByte(); byte mes = inFile.nextByte(); int ano = inFile.nextInt();
-                        String consultaRealizada = inFile.next();
+                        inFile.nextLine();
+                        String consultaRealizada = inFile.nextLine();
+                        if(consultaRealizada.equals("s")) consultaRealizada = "Sim";
+                        else consultaRealizada = "Não";
                     
-                        System.out.printf("CPF do paciente: %s%nData da consulta: %d/%d/%d%nConsulta realizada? ",
+                        System.out.println("---------------Registro #" + ++it + "---------------");
+                        System.out.printf("CPF do paciente: %s%nData da consulta: %d/%d/%d%nConsulta realizada? %s%n",
                         cpfPac, dia, mes, ano, consultaRealizada);
+                        System.out.println();
                     }
                 break;
                 case 4:
                     while(inFile.hasNext()){
-                        String cpfPront = inFile.next();
+                        String cpfPront = inFile.nextLine();
                         String sintomas = inFile.nextLine();
                         String diag = inFile.nextLine();
                     
+                        System.out.println("---------------Registro #" + ++it + "---------------");
                         System.out.printf("CPF do paciente: %s%nSintomas: %s%nDagnóstico: %s%n",
                         cpfPront, sintomas, diag);
+                        System.out.println();
                     }
                 break;
             }
