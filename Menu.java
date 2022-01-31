f90e8a1abaef821aae2650e80e12651dc7733084
David Machado <78983718+Davidmachadocb@users.noreply.github.com>
2022-01-31 12:19:15 -0300

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,40 @@
+public class Menu {
+
+    public static void menu_principal(){
+
+
+        System.out.println("\n\n                         #### Clinica ####");
+		System.out.println("\n                  =====================================");
+		System.out.println("                  |     1 - Menu do Paciente          |");
+		System.out.println("                  |     2 - Menu do Especialista      |");
+		System.out.println("                  |     0 - Sair                      |");
+		System.out.println("                  =====================================\n"); 
+    }
+
+
+    public static void menu_paciente(){
+
+
+        System.out.println("\n\n                    #### Clinica - Menu Paciente ####");
+		System.out.println("\n                  ========================================");
+		System.out.println("                  |     1 - Pesquisar Paciente           |");
+		System.out.println("                  |     2 - Marcar Consulta              |");
+		System.out.println("                  |     3 - Cadastrar Paciente           |");
+		System.out.println("                  |     4 - Remover Paciente             |");
+		System.out.println("                  |     0 - Voltar                       |");
+		System.out.println("                  ========================================\n");
+    }
+
+    public static void menu_especialista(){
+
+
+        System.out.println("\n\n                    #### Clinica - Menu Especialista ####");
+		System.out.println("\n                  ============================================");
+		System.out.println("                  |     1 - Pesquisar Especialista           |");
+		System.out.println("                  |     3 - Cadastrar Especialista           |");
+		System.out.println("                  |     4 - Remover Especialista             |");
+		System.out.println("                  |     0 - Voltar                           |");
+		System.out.println("                  ============================================\n");
+    }
+
+}
