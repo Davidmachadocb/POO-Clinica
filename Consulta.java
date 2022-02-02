7e8090a9c216855e7ec9969b473f8bb87b275380
Francisco-Italo <65195244+Francisco-Italo@users.noreply.github.com>
2022-02-02 16:51:47 -0300

    Update Consulta.java

--- Consulta.java
+++ Consulta.java
@@ -1,18 +1,20 @@
-public class Consulta {
+public class Consulta implements Comparable<Consulta> {
     
     private String cpf_paciente;
     private Data dataOcur;
+    private Hora horaCon;
     private boolean realizada;
+    private String crmDoc;
     
-    public Consulta(String cpf, Data dataOcur, boolean realizada){
+    public Consulta(String cpf, Data dataOcur, Hora horaCon, boolean realizada, String crmDoc){
 
         this.cpf_paciente = cpf;
         this.dataOcur = dataOcur;
+        this.horaCon = horaCon;
         this.realizada = realizada;
+        this.crmDoc = crmDoc;
 
     }
-
-    
 
     public String getCpf_paciente() {
         return cpf_paciente;
@@ -26,6 +28,14 @@ public class Consulta {
         return realizada;
     }
 
+    public String getCrmDoc() {
+        return crmDoc;
+    }
+
+    public Hora getHoraCon() {
+        return horaCon;
+    }
+
     public void setRealizada(boolean realizada) {
         this.realizada = realizada;
     }
@@ -33,14 +43,39 @@ public class Consulta {
     public void setCpf_paciente(String cpf_paciente) {
         this.cpf_paciente = cpf_paciente;
     }
+
+    public void setHoraCon(Hora horaCon) {
+        this.horaCon = horaCon;
+    }
     
     public void setDataOcur(Data dataOcur) {
         this.dataOcur = dataOcur;
     }
 
-    @Override
-    public String toString() {
-        return String.format("CPF: %s\n", this.cpf_paciente) + this.dataOcur.toString() + String.format("Realizada: %b\n", this.realizada);
+    public void setCrmDoc(String crmDoc) {
+        this.crmDoc = crmDoc;
     }
 
+    @Override public int compareTo(Consulta con){
+        int ordering = Integer.compare(this.dataOcur.getAno(), con.dataOcur.getAno());
+        if(ordering == 0){
+            ordering = Integer.compare(this.dataOcur.getMes(), con.dataOcur.getMes());
+            if(ordering == 0){
+                ordering = Integer.compare(this.dataOcur.getDia(), con.dataOcur.getDia());
+                if(ordering == 0){
+                    ordering = Integer.compare(this.horaCon.getHora(), con.horaCon.getHora());
+                    if(ordering == 0){
+                        return Integer.compare(this.horaCon.getMin(), con.horaCon.getMin());
+                    }else return ordering;
+                }else return ordering;
+            }else return ordering;
+        }else return ordering;
+    }
+
+    @Override
+    public String toString() {
+    return String.format("CPF: %s\n", this.cpf_paciente) +
+           "Data da consulta: " + this.dataOcur.toString() + "\n" + "Horário: " + this.horaCon.toString() + "\n" +
+           String.format("Realizada? %s\nCRM médico: %s\n", this.realizada, this.crmDoc);
+    }
 }
