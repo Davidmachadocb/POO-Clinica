42287628aa22368bf8aa060765c8ad0f5eff35e8
Francisco-Italo <65195244+Francisco-Italo@users.noreply.github.com>
2022-02-03 17:50:53 -0300

    Update fileRead.java

--- fileRead.java
+++ fileRead.java
@@ -5,88 +5,31 @@ import java.nio.charset.StandardCharsets;
 import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 class fileRead {
-    public static void readWichFile(byte id){
+
+    public static HashMap<String, Paciente> readFilePaciente(){
         Locale.setDefault(Locale.US);
         String filePaciente = "/home/itsfrancisco/Área de Trabalho/Pacientes.txt";
-        String fileClinico = "/home/itsfrancisco/Área de Trabalho/Medicos.txt";
-        String fileConsulta = "/home/itsfrancisco/Área de Trabalho/Consultas.txt";
-        String fileProntuario = "/home/itsfrancisco/Área de Trabalho/Prontuarios.txt";
+        File file = new File(filePaciente);
 
-        String fileSelected = "";
-        switch(id){
-            case 1: fileSelected = filePaciente; break;
-            case 2: fileSelected = fileClinico; break;
-            case 3: fileSelected = fileConsulta; break;
-            case 4: fileSelected = fileProntuario; break;
-            default: System.err.println("ID inválido."); return;
-        }
-
-        File file = new File(fileSelected);
-
+        HashMap<String, Paciente> mapping = new HashMap<>();
         try(Scanner inFile = new Scanner(file, StandardCharsets.UTF_8);){
-            int it = 0;
-            switch(id){
-                case 1:
-                    while(inFile.hasNext()){
-                        String name = inFile.nextLine();
-                        String endereco = inFile.nextLine();
-                        String numeroTel = inFile.nextLine();
-                        String cpf = inFile.nextLine();
-                        byte dia = inFile.nextByte(); byte mes = inFile.nextByte(); int ano = inFile.nextInt();
-                        inFile.nextLine();
-                        String sexo = inFile.nextLine();
-                        String natu = inFile.nextLine();
-                        
-                        System.out.println("---------------Registro #" + ++it + "---------------");
-                        System.out.printf("Nome completo: %s%nEndereço: %s%nTelefone: %s%nCPF: %s%nData de nascimento: %d/%d/%d%nSexo: %s%nNaturalidade: %s%n",
-                        name, endereco, numeroTel, cpf, dia, mes, ano, sexo, natu);
-                        System.out.println();
-                    }
-                break;
-                case 2:
-                    while(inFile.hasNext()){
-                        String name = inFile.nextLine();
-                        String endereco = inFile.nextLine();
-                        String numeroTel = inFile.nextLine();
-                        String reg = inFile.nextLine();
-                        String spec = inFile.nextLine();
-                    
-                        System.out.println("---------------Registro #" + ++it + "---------------");
-                        System.out.printf("Nome completo: %s%nEndereço: %s%nTelefone: %s%nCRM: %s%nEspecialidade: %s%n",
-                        name, endereco, numeroTel, reg, spec);
-                        System.out.println();
-                    }
-                break;
-                case 3:
-                    while(inFile.hasNext()){
-                        String cpfPac = inFile.nextLine();
-                        byte dia = inFile.nextByte(); byte mes = inFile.nextByte(); int ano = inFile.nextInt();
-                        inFile.nextLine();
-                        String consultaRealizada = inFile.nextLine();
-                        if(consultaRealizada.equals("s")) consultaRealizada = "Sim";
-                        else consultaRealizada = "Não";
-                    
-                        System.out.println("---------------Registro #" + ++it + "---------------");
-                        System.out.printf("CPF do paciente: %s%nData da consulta: %d/%d/%d%nConsulta realizada? %s%n",
-                        cpfPac, dia, mes, ano, consultaRealizada);
-                        System.out.println();
-                    }
-                break;
-                case 4:
-                    while(inFile.hasNext()){
-                        String cpfPront = inFile.nextLine();
-                        String sintomas = inFile.nextLine();
-                        String diag = inFile.nextLine();
-                    
-                        System.out.println("---------------Registro #" + ++it + "---------------");
-                        System.out.printf("CPF do paciente: %s%nSintomas: %s%nDagnóstico: %s%n",
-                        cpfPront, sintomas, diag);
-                        System.out.println();
-                    }
-                break;
-            }
+            while(inFile.hasNext()){
+                String name = inFile.nextLine();
+                String endereco = inFile.nextLine();
+                String numeroTel = inFile.nextLine();
+                String cpf = inFile.nextLine();
+                byte dia = inFile.nextByte(); byte mes = inFile.nextByte(); int ano = inFile.nextInt();
+                inFile.nextLine();
+                char sexo = inFile.next().charAt(0); inFile.nextLine();
+                String natu = inFile.nextLine();
+                
+                Paciente p = new Paciente(name, endereco, numeroTel, cpf, new Data(dia, mes, ano), sexo, natu);
+                mapping.put(cpf, p);
+            }   
         }catch(InputMismatchException e) {
             System.out.println("Entrada inválida.");
         }catch(FileNotFoundException e) {
@@ -94,5 +37,94 @@ class fileRead {
         }catch(IOException e) {
             System.err.println("Exceção de E/S");
         }
+        return mapping;
     }
+
+    public static HashMap<String, Clinico> readFileClinico(){
+        Locale.setDefault(Locale.US);
+        String fileClinico = "/home/itsfrancisco/Área de Trabalho/Medicos.txt";
+        File file = new File(fileClinico);
+
+        HashMap<String, Clinico> mapping = new HashMap<>();
+        try(Scanner inFile = new Scanner(file, StandardCharsets.UTF_8);){
+            while(inFile.hasNext()){
+                while(inFile.hasNext()){
+                    String name = inFile.nextLine();
+                    String endereco = inFile.nextLine();
+                    String numeroTel = inFile.nextLine();
+                    String reg = inFile.nextLine();
+                    String spec = inFile.nextLine();
+                
+                    Clinico c = new Clinico(name, endereco, numeroTel, reg, spec);
+                    mapping.put(reg, c);
+                }
+            }   
+        }catch(InputMismatchException e) {
+            System.out.println("Entrada inválida.");
+        }catch(FileNotFoundException e) {
+            System.err.println("Arquivo não encontrado.");
+        }catch(IOException e) {
+            System.err.println("Exceção de E/S");
+        }
+
+        return mapping;
+    }
+
+    /*public static HashMap<String, ArrayList<Consulta>> readFileConsulta(){
+        Locale.setDefault(Locale.US);
+        String fileConsulta = "/home/itsfrancisco/Área de Trabalho/Consultas.txt";
+        File file = new File(fileConsulta);
+
+        HashMap<String, Consulta> mapping = new HashMap<>();
+        try(Scanner inFile = new Scanner(file, StandardCharsets.UTF_8);){
+            while(inFile.hasNext()){
+                while(inFile.hasNext()){
+                    String name = inFile.nextLine();
+                    String endereco = inFile.nextLine();
+                    String numeroTel = inFile.nextLine();
+                    String reg = inFile.nextLine();
+                    String spec = inFile.nextLine();
+                
+                    Clinico c = new Clinico(name, endereco, numeroTel, reg, spec);
+                    
+                }
+            }   
+        }catch(InputMismatchException e) {
+            System.out.println("Entrada inválida.");
+        }catch(FileNotFoundException e) {
+            System.err.println("Arquivo não encontrado.");
+        }catch(IOException e) {
+            System.err.println("Exceção de E/S");
+        }
+
+        return mapping;
+    }
+
+    public static HashMap<String, ArrayList<Prontuario>> readFileProntuario(){
+        Locale.setDefault(Locale.US);
+        String fileProntuario = "/home/itsfrancisco/Área de Trabalho/Prontuarios.txt";
+        File file = new File(fileProntuario);
+
+        HashMap<String, ArrayList<Prontuario>> mapping = new HashMap<>();
+        try(Scanner inFile = new Scanner(file, StandardCharsets.UTF_8);){
+            while(inFile.hasNext()){
+                while(inFile.hasNext()){
+                    String cpfPacPront = inFile.nextLine();
+                    String sintomas = inFile.nextLine();
+                    String diag = inFile.nextLine();
+                
+                    Prontuario pront = new Prontuario(cpfPacPront, sintomas, diag);
+                    mapping.put(cpfPacPront, pront);
+                }
+            }   
+        }catch(InputMismatchException e) {
+            System.out.println("Entrada inválida.");
+        }catch(FileNotFoundException e) {
+            System.err.println("Arquivo não encontrado.");
+        }catch(IOException e) {
+            System.err.println("Exceção de E/S");
+        }
+
+        return mapping;
+    }*/    
 }
