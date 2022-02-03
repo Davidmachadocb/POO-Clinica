50a557e157c76d73178b59b89ee5f8c1c86cf1cc
David Machado <78983718+Davidmachadocb@users.noreply.github.com>
2022-02-03 11:34:28 -0300

    Update Clinica.java

--- Clinica.java
+++ Clinica.java
@@ -1,188 +1,117 @@
+import java.util.HashMap;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Collections;
 
-public class Principal{
+public class Clinica {
+    private HashMap<String, Clinico> clinicos;
+    private HashMap<String, Paciente> pacientes;
+    private HashMap<String, ArrayList<Consulta>> consultas;
+    private HashMap<String, ArrayList<Prontuario>> prontuarios;
 
-    public static void Second_option(Clinica clinica){
-
-        Scanner kb = new Scanner(System.in);
-        int option_2;
-        String cpf, nome, telefone, endereço, naturalidade;
-        char sex;
-        Byte dia, mes;
-        short ano;
-        Data nasc;
-        do{
+    public Clinica(){
         
-            System.out.print("\033[H\033[2J");
-
-            Menu.menu_paciente();
-            option_2 = Integer.parseInt(System.console().readLine("Opção -> "));
-            switch (option_2) {
-                
-                case 0:
-                    break;
-                
-                case 1:
-                    cpf = System.console().readLine("CPF do paciente: ");
-                    if(clinica.Pesqpaciente(cpf) == null){
-                        System.out.println("Paciente não foi cadastrado.");
-                        System.console().readLine("Digite qualquer tecla pra continuar: ");
-                        break;
-                    }
-                    System.out.println(clinica.Pesqpaciente(cpf).toString());
-                    System.console().readLine("Digite qualquer tecla pra continuar: ");
-                    break;
-                
-                case 2:
-					System.out.print("Insira o CPF da paciente: ");
-					String cpfPac = kb.nextLine();
-
-					System.out.print("Data para consulta (dd mm aaaa): ");
-					dia = kb.nextByte(); mes = kb.nextByte(); ano = kb.nextShort();
-
-					System.out.print("Horário para consulta: ");
-					byte hora = kb.nextByte(); byte min = kb.nextByte();
-
-					System.out.print("Médico responsável: ");
-					String crmDoc = kb.nextLine();
-					
-					Consulta consMark = new Consulta(cpfPac, new Data(dia, mes, ano), new Hora(hora, min), false, crmDoc);
-
-					//clinica.addConsulta(consMark);
-				    break;
-
-                case 3:
-
-                    nome = System.console().readLine("Nome completo: ");
-                    cpf = System.console().readLine("CPF: ");
-                    dia = Byte.parseByte(System.console().readLine("Dia de nascimento: "));
-                    mes = Byte.parseByte(System.console().readLine("mês de nascimento: "));
-                    ano = Short.parseShort(System.console().readLine("Ano de nascimento: "));
-                    nasc = new Data(dia, mes, ano);
-                    sex = System.console().readLine("Sexo: ").charAt(0);
-                    endereço = System.console().readLine("Endereço: ");
-                    telefone = System.console().readLine("Número de Telefone: ");
-                    naturalidade = System.console().readLine("Naturalidade: ");
-
-                    Paciente newp = new Paciente(nome, endereço, telefone, cpf, nasc, sex, naturalidade);
-
-                    clinica.addPaciente(newp);
-
-                    break;
-
-                case 4:
-                    cpf = System.console().readLine("CPF do paciente: ");
-                    clinica.delPaciente(cpf);
-                    System.console().readLine("Digite qualquer tecla pra continuar: ");
-                    System.out.print("\033[H\033[2J");
-                    break;
-
-                default:
-                    System.out.println("Opção Inválida");
-                    break;
-            }
-        }while(option_2 != 0);
-        
+        this.clinicos = new HashMap<>();
+        this.pacientes = new HashMap<>();
+        this.consultas = new HashMap<>();
+        this.prontuarios = new HashMap<>();
 
     }
 
-    public static void Third_option(Clinica clinica){
-        int option_3;
-        String registro, nome, telefone, endereço, espec;
+    public boolean addPaciente(Paciente newp){
 
-        do{
+        String new_cpf = newp.getCpf();
 
-            System.out.print("\033[H\033[2J");
-
-            Menu.menu_especialista();
-            option_3 = Integer.parseInt(System.console().readLine("Opção -> "));
-            switch (option_3) {
-                case 0:
-                    break;
-                
-                case 1:
-                    registro = System.console().readLine("Registro do especialista: ");
-                    System.out.println(clinica.Pesqclinico(registro).toString());
-                    System.console().readLine("Digite qualquer tecla pra continuar: ");
-                    break;
-
-                case 2:
-                    nome = System.console().readLine("Nome completo: ");
-                    registro = System.console().readLine("Registro: ");
-                    endereço = System.console().readLine("Endereço: ");
-                    telefone = System.console().readLine("Número de Telefone: ");
-                    espec = System.console().readLine("Especialidade: ");
-
-
-                    Clinico newc = new Clinico(nome, endereço, telefone, registro, espec);
-
-                    clinica.addClinico(newc);
-
-                    break;
-
-                case 3:
-                    registro = System.console().readLine("Registro do especialista: ");
-                    clinica.delPaciente(registro);
-                    System.console().readLine("Digite qualquer tecla pra continuar: ");
-                    break;
-
-                case 4:
-                    String crmResp = System.console().readLine("CRM do médico responsável: ");
-
-                    List<Consulta> consulta = new ArrayList<>(); 
-                    consulta = clinica.getConsultas().get(crmResp);
-
-                    Collections.sort(consulta);
-
-                    for(Consulta aux : consulta){
-                        System.out.println(aux.toString());
-                    }
-                    System.console().readLine("Digite qualquer tecla pra continuar: ");
-
-                    break;
-
-                default:
-                    System.out.println("Opção Inválida");
-                    break;
-            }
-        }while(option_3 != 0);
-    }
-
-    public static void main(String[] args) {
-
-        Clinica clinica = new Clinica(); 
-        int option;
-        do{
-            System.out.print("\033[H\033[2J");
-            
-            Menu.menu_principal();
-
-            option = Integer.parseInt(System.console().readLine("Opção -> "));
-
-            switch(option){
-                case 0:
-                    System.out.println("Programa Finalizado!");
-                    break;
-
-                case 1:
-                    System.out.print("\033[H\033[2J");
-                    Second_option(clinica);
-                    break;
-            
-                case 2:
-                    System.out.print("\033[H\033[2J");
-                    Third_option(clinica);
-                    break;
-
-                default:
-                    System.out.println("Opção Inválida");
-                    break;
-            }
-
-        }while(option != 0);
+        if(this.pacientes.containsKey(new_cpf)){
+            System.out.println("Paciente já cadastrado.");
+            return false;
+        }
+        this.pacientes.put(new_cpf, newp);
+        return true;
 
     }
+
+    public boolean addClinico(Clinico newc){
+        String new_registro = newc.getRegistro();
+
+        if(this.pacientes.containsKey(new_registro)){
+            System.out.println("Clinico já cadastrado.");
+            return false;
+        }
+        this.clinicos.put(new_registro, newc);
+        ArrayList<Consulta> clinico_Consultas = new ArrayList<Consulta>();
+        this.consultas.put(new_registro, clinico_Consultas);
+        return true;
+    }
+
+    public boolean addConsulta(Consulta consMark){
+
+        this.consultas.get(consMark.getCrmDoc()).add(consMark);
+        return true;
+
+    }
+
+    public Paciente Pesqpaciente(String cpf){
+
+        if(this.pacientes.containsKey(cpf)){
+            return this.pacientes.get(cpf);
+        }
+
+        return null;
+    }
+
+    public Clinico Pesqclinico(String registro){
+
+        if(this.clinicos.containsKey(registro)){
+            return this.clinicos.get(registro);
+        }
+
+        return null;
+    }
+
+    public HashMap<String, Clinico> getClinicos() {
+        return clinicos;
+    }
+
+    public HashMap<String, Paciente> getPacientes() {
+        return pacientes;
+    }
+
+    public HashMap<String, ArrayList<Consulta>> getConsultas() {
+        return consultas;
+    }
+
+    public void setClinicos(HashMap<String, Clinico> clinicos) {
+        this.clinicos = clinicos;
+    }
+
+    public void setPacientes(HashMap<String, Paciente> pacientes) {
+        this.pacientes = pacientes;
+    }
+
+    /*public boolean cancelarConsulta(String cpfPac){
+
+    }*/
+
+    public boolean delPaciente(String cpfPac){
+
+        if(this.pacientes.containsKey(cpfPac)){
+            this.pacientes.remove(cpfPac);
+            System.out.println("Paciente removido.");
+            return true;
+        }
+        System.out.println("Paciente não encontrado");
+        return false;
+    }
+
+    public boolean delClinico(String reg){
+
+        if(this.pacientes.containsKey(reg)){
+            this.clinicos.remove(reg);
+            System.out.println("Clinico removido.");
+            return true;
+        }
+        System.out.println("Clinico não encontrado");
+        return false;
+    }
+    
+    //public boolean delProntuario(String cpfPac){}
 }
