c177bc520efb9326d68c89c3825f09563120fb84
David Machado <78983718+Davidmachadocb@users.noreply.github.com>
2022-02-02 23:01:59 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,41 +1,188 @@
-public class Menu {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Collections;
 
-    public static void menu_principal(){
+public class Principal{
 
+    public static void Second_option(Clinica clinica){
 
-        System.out.println("\n\n         #### Clinica ####\n");
-		System.out.println("=====================================");
-		System.out.println("|     1 - Menu do Paciente          |");
-		System.out.println("|     2 - Menu do Especialista      |");
-		System.out.println("|     0 - Sair                      |");
-		System.out.println("=====================================\n"); 
+        Scanner kb = new Scanner(System.in);
+        int option_2;
+        String cpf, nome, telefone, endereço, naturalidade;
+        char sex;
+        Byte dia, mes;
+        short ano;
+        Data nasc;
+        do{
+        
+            System.out.print("\033[H\033[2J");
+
+            Menu.menu_paciente();
+            option_2 = Integer.parseInt(System.console().readLine("Opção -> "));
+            switch (option_2) {
+                
+                case 0:
+                    break;
+                
+                case 1:
+                    cpf = System.console().readLine("CPF do paciente: ");
+                    if(clinica.Pesqpaciente(cpf) == null){
+                        System.out.println("Paciente não foi cadastrado.");
+                        System.console().readLine("Digite qualquer tecla pra continuar: ");
+                        break;
+                    }
+                    System.out.println(clinica.Pesqpaciente(cpf).toString());
+                    System.console().readLine("Digite qualquer tecla pra continuar: ");
+                    break;
+                
+                case 2:
+					System.out.print("Insira o CPF da paciente: ");
+					String cpfPac = kb.nextLine();
+
+					System.out.print("Data para consulta (dd mm aaaa): ");
+					dia = kb.nextByte(); mes = kb.nextByte(); ano = kb.nextShort();
+
+					System.out.print("Horário para consulta: ");
+					byte hora = kb.nextByte(); byte min = kb.nextByte();
+
+					System.out.print("Médico responsável: ");
+					String crmDoc = kb.nextLine();
+					
+					Consulta consMark = new Consulta(cpfPac, new Data(dia, mes, ano), new Hora(hora, min), false, crmDoc);
+
+					//clinica.addConsulta(consMark);
+				    break;
+
+                case 3:
+
+                    nome = System.console().readLine("Nome completo: ");
+                    cpf = System.console().readLine("CPF: ");
+                    dia = Byte.parseByte(System.console().readLine("Dia de nascimento: "));
+                    mes = Byte.parseByte(System.console().readLine("mês de nascimento: "));
+                    ano = Short.parseShort(System.console().readLine("Ano de nascimento: "));
+                    nasc = new Data(dia, mes, ano);
+                    sex = System.console().readLine("Sexo: ").charAt(0);
+                    endereço = System.console().readLine("Endereço: ");
+                    telefone = System.console().readLine("Número de Telefone: ");
+                    naturalidade = System.console().readLine("Naturalidade: ");
+
+                    Paciente newp = new Paciente(nome, endereço, telefone, cpf, nasc, sex, naturalidade);
+
+                    clinica.addPaciente(newp);
+
+                    break;
+
+                case 4:
+                    cpf = System.console().readLine("CPF do paciente: ");
+                    clinica.delPaciente(cpf);
+                    System.console().readLine("Digite qualquer tecla pra continuar: ");
+                    System.out.print("\033[H\033[2J");
+                    break;
+
+                default:
+                    System.out.println("Opção Inválida");
+                    break;
+            }
+        }while(option_2 != 0);
+        
+
     }
 
+    public static void Third_option(Clinica clinica){
+        int option_3;
+        String registro, nome, telefone, endereço, espec;
 
-    public static void menu_paciente(){
+        do{
 
-		System.out.println("\n");
-        System.out.println("    #### Clinica - Menu Paciente ####\n");
-		System.out.println("========================================");
-		System.out.println("|     1 - Pesquisar Paciente           |");
-		System.out.println("|     2 - Marcar Consulta              |");
-		System.out.println("|     3 - Cadastrar Paciente           |");
-		System.out.println("|     4 - Remover Paciente             |");
-		System.out.println("|     0 - Voltar                       |");
-		System.out.println("========================================\n");
+            System.out.print("\033[H\033[2J");
+
+            Menu.menu_especialista();
+            option_3 = Integer.parseInt(System.console().readLine("Opção -> "));
+            switch (option_3) {
+                case 0:
+                    break;
+                
+                case 1:
+                    registro = System.console().readLine("Registro do especialista: ");
+                    System.out.println(clinica.Pesqclinico(registro).toString());
+                    System.console().readLine("Digite qualquer tecla pra continuar: ");
+                    break;
+
+                case 2:
+                    nome = System.console().readLine("Nome completo: ");
+                    registro = System.console().readLine("Registro: ");
+                    endereço = System.console().readLine("Endereço: ");
+                    telefone = System.console().readLine("Número de Telefone: ");
+                    espec = System.console().readLine("Especialidade: ");
+
+
+                    Clinico newc = new Clinico(nome, endereço, telefone, registro, espec);
+
+                    clinica.addClinico(newc);
+
+                    break;
+
+                case 3:
+                    registro = System.console().readLine("Registro do especialista: ");
+                    clinica.delPaciente(registro);
+                    System.console().readLine("Digite qualquer tecla pra continuar: ");
+                    break;
+
+                case 4:
+                    String crmResp = System.console().readLine("CRM do médico responsável: ");
+
+                    List<Consulta> consulta = new ArrayList<>(); 
+                    consulta = clinica.getConsultas().get(crmResp);
+
+                    Collections.sort(consulta);
+
+                    for(Consulta aux : consulta){
+                        System.out.println(aux.toString());
+                    }
+                    System.console().readLine("Digite qualquer tecla pra continuar: ");
+
+                    break;
+
+                default:
+                    System.out.println("Opção Inválida");
+                    break;
+            }
+        }while(option_3 != 0);
     }
 
-    public static void menu_especialista(){
+    public static void main(String[] args) {
 
-		System.out.println("\n");
-        System.out.println("     #### Clinica - Menu Especialista ####\n");
-		System.out.println("============================================");
-		System.out.println("|     1 - Pesquisar Especialista           |");
-		System.out.println("|     2 - Cadastrar Especialista           |");
-		System.out.println("|     3 - Remover Especialista             |");
-		System.out.println("|     4 - Consultas                        |");
-		System.out.println("|     0 - Voltar                           |");
-		System.out.println("============================================\n");
+        Clinica clinica = new Clinica(); 
+        int option;
+        do{
+            System.out.print("\033[H\033[2J");
+            
+            Menu.menu_principal();
+
+            option = Integer.parseInt(System.console().readLine("Opção -> "));
+
+            switch(option){
+                case 0:
+                    System.out.println("Programa Finalizado!");
+                    break;
+
+                case 1:
+                    System.out.print("\033[H\033[2J");
+                    Second_option(clinica);
+                    break;
+            
+                case 2:
+                    System.out.print("\033[H\033[2J");
+                    Third_option(clinica);
+                    break;
+
+                default:
+                    System.out.println("Opção Inválida");
+                    break;
+            }
+
+        }while(option != 0);
+
     }
-
 }
